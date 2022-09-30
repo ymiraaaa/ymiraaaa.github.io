3ff99b54ae0c6885f558bdf70e4416163930311c
stev <angelsalsaw@gmail.com>
2022-09-30 15:54:34 +0700

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,10 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
+$(window).on('load', function () {
+    $('#loading').hide();
+  }) 
+  
 (function($) {
 
 	var	$window = $(window),
